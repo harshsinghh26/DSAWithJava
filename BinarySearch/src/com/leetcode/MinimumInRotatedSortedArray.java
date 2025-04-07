1e7444d94a82d52ada5f4cddb9237f52d2f79484
harshsinghh26 <harshsi343@gmail.com>
2025-04-07 13:16:51 +0530

    add minimumInRotatedSortedArray question

--- BinarySearch/src/com/leetcode/MinimumInRotatedSortedArray.java
+++ BinarySearch/src/com/leetcode/MinimumInRotatedSortedArray.java
@@ -1,30 +1,26 @@
 package com.leetcode;
 
-public class RotationCount {
+public class MinimumInRotatedSortedArray {
     public static void main(String[] args) {
-        int[] arr = {2, 3, 6, 12};
-        System.out.println(rotationCount(arr));
+        int[] nums = {4,5,6,7,0,1,2};
+        System.out.println(findMin(nums));
     }
-    static int rotationCount(int[] nums) {
-        int pivot = findPivot(nums);
-        return pivot + 1;
-    }
-    static int findPivot(int[] arr){
+    static int findMin(int[] arr) {
         int s = 0;
         int e = arr.length - 1;
 
         while(s <= e){
             int mid = s + (e - s) / 2;
             if(mid < e && arr[mid] > arr[mid + 1]){
-                return mid;
+                return arr[mid + 1];
             }if(mid > s && arr[mid] < arr[mid - 1]){
-                return mid - 1;
+                return arr[mid];
             }if(arr[mid] < arr[s]){
                 e = mid -1;
             }else {
                 s = mid + 1;
             }
         }
-        return -1;
+        return arr[0];
     }
 }
