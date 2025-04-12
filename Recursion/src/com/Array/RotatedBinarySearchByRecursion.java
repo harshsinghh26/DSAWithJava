8ad584435e6d303a9c75b39628009a9775b50520
harshsinghh26 <harshsi343@gmail.com>
2025-04-12 13:01:28 +0530

    added rotated binary search by rescursion

---
+++ Recursion/src/com/Array/RotatedBinarySearchByRecursion.java
@@ -0,0 +1,30 @@
+package com.Array;
+
+public class RotatedBinarySearchByRecursion {
+    public static void main(String[] args) {
+        int[] arr = {5, 6, 7, 8, 1, 2, 3, 4};
+        int target = 4;
+        System.out.println(search(arr, target, 0, arr.length-1));
+    }
+    static int search(int[] arr, int target, int s, int e){
+        if (s > e){
+            return -1;
+        }
+
+        int mid = s + (e -s)/2;
+        if (target == arr[mid]){
+            return mid;
+        }
+        if (arr[s] <= arr[mid]){
+            if (target >= arr[s] && target <= arr[mid]){
+                return search(arr, target, s, mid -1);
+            }else {
+                return search(arr, target, mid + 1, e);
+            }
+        }
+        if (target >= arr[mid] && target <= arr[e]){
+            return search(arr, target, mid+1, e);
+        }
+        return search(arr, target, s, mid -1);
+    }
+}
