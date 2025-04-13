f3fb61e30a3402a8d722362a1807c739301f8604
harshsinghh26 <harshsi343@gmail.com>
2025-04-13 12:37:18 +0530

    Reverse String question added

---
+++ Recursion/src/com/easy/ReverseStringArray.java
@@ -0,0 +1,19 @@
+package com.easy;
+
+public class ReverseStringArray {
+    public static void main(String[] args) {
+
+    }
+    static void reverseString(char[] s) {
+        helper(s, 0, s.length -1);
+    }
+    static void helper(char[] s, int start, int end){
+        if(start >= end){
+            return;
+        }
+        char temp = s[start];
+        s[start] = s[end];
+        s[end] = temp;
+        helper(s, start + 1, end -1);
+    }
+}
