db893847a33157bd15ccbd00d7be33ca79655f54
harshsinghh26 <harshsi343@gmail.com>
2025-04-17 14:14:37 +0530

    added backtracking with river

--- Recursion/src/com/MazeProblems/BackTracking.java
+++ Recursion/src/com/MazeProblems/BackTracking.java
@@ -7,9 +7,9 @@ public class BackTracking {
         boolean[][] maze = {
                 {true, true, true},
                 {true, true, true},
-                {true, true, true}
         };
-        System.out.println(mazeWithRiverPathA("", maze, 0, 0));
+        mazeWithRiverPath("", maze, 0, 0);
+//        System.out.println(mazeWithRiverPathA("", maze, 0, 0));
     }
     static ArrayList<String> mazeWithRiverPathA(String p, boolean[][] maze, int r, int c){
         if (r == maze.length - 1 && c == maze[0].length - 1){
@@ -38,5 +38,31 @@ public class BackTracking {
         }
         maze[r][c] = true;
         return ans;
+    }
+
+    static void mazeWithRiverPath(String p, boolean[][] maze, int r, int c){
+        if (r == maze.length - 1 && c == maze[0].length - 1){
+            System.out.println(p);
+            return;
+        }
+
+        if (!maze[r][c]){
+            return;
+        }
+
+        maze[r][c] = false;
+        if (r < maze.length -1){
+            mazeWithRiverPath(p + "D", maze, r +1, c);
+        }
+        if (c < maze[0].length - 1){
+            mazeWithRiverPath(p + "R", maze, r, c+1);
+        }
+        if (r > 0){
+            mazeWithRiverPath(p + "U", maze, r-1, c);
+        }
+        if (c > 0){
+            mazeWithRiverPath(p + "L", maze, r, c-1);
+        }
+        maze[r][c] = true;
     }
 }
