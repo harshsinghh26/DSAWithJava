e3b7c6abdc1ed46ec8e2f99be5f51d2b94669c2e
harshsinghh26 <harshsi343@gmail.com>
2025-04-17 11:53:17 +0530

    added maze problem

--- Recursion/src/com/MazeProblems/MazeProblem.java
+++ Recursion/src/com/MazeProblems/MazeProblem.java
@@ -1,11 +1,14 @@
 package com.MazeProblems;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class MazeProblem {
     public static void main(String[] args) {
 //        System.out.println(maze(3, 3));
-        maze("", 3, 3);
+//        maze("", 3, 3);
+//        System.out.println(mazeA("", 3, 3));
+        System.out.println(mazeAD("", 3, 3));
     }
     static int maze(int r, int c){
         if (r == 1 || c == 1){
@@ -30,5 +33,46 @@ public class MazeProblem {
             maze(p + "R", r, c - 1);
         }
     }
+
+    // add it in array list
+
+    static ArrayList<String> mazeA(String p, int r, int c){
+        if (r == 1 && c == 1){
+            ArrayList<String> list = new ArrayList<>();
+            list.add(p);
+            return list;
+        }
+        ArrayList<String> ans = new ArrayList<>();
+        if (r > 1){
+            ans.addAll(mazeA(p + "D",r-1, c));
+        }
+        if (c > 1) {
+            ans.addAll(mazeA(p + "R", r, c - 1));
+        }
+        return ans;
+    }
+
+
+    // can go diagonal Also
+
+    static ArrayList<String> mazeAD(String p, int r, int c){
+        if (r == 1 && c == 1){
+            ArrayList<String> list = new ArrayList<>();
+            list.add(p);
+            return list;
+        }
+        ArrayList<String> ans = new ArrayList<>();
+        if (r > 1 && c > 1){
+            ans.addAll(mazeAD(p + 'D', r-1, c -1));
+        }
+        if (r > 1){
+            ans.addAll(mazeAD(p + "V",r-1, c));
+        }
+        if (c > 1) {
+            ans.addAll(mazeAD(p + "H", r, c - 1));
+        }
+        return ans;
+    }
+
 
 }
