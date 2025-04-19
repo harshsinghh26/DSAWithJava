064dfb1368072a3dde8a2fe963d450e381f70e63
harshsinghh26 <harshsi343@gmail.com>
2025-04-19 14:43:25 +0530

    added without taking arg in gicc problem only take n inarg

--- Recursion/src/com/easy/Gicconaaci.java
+++ Recursion/src/com/easy/Gicconaaci.java
@@ -4,17 +4,18 @@ import java.util.Scanner;
 
 public class Gicconaaci {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int T = sc.nextInt();  // number of test cases
-
-        while (T-- > 0) {
-            int A = sc.nextInt();
-            int B = sc.nextInt();
-            int C = sc.nextInt();
-            int N = sc.nextInt();
-
-            System.out.println(geekonacci(A, B, C, N));
-        }
+//        Scanner sc = new Scanner(System.in);
+//        int T = sc.nextInt();  // number of test cases
+//
+//        while (T-- > 0) {
+//            int A = sc.nextInt();
+//            int B = sc.nextInt();
+//            int C = sc.nextInt();
+//            int N = sc.nextInt();
+//
+//            System.out.println(geekonacci(A, B, C, N));
+//        }
+        System.out.println(gic(7));
     }
     static int geekonacci(int A, int B, int C, int N) {
         if (N == 1) return A;
@@ -22,5 +23,15 @@ public class Gicconaaci {
         if (N == 3) return C;
 
         return geekonacci(A, B, C, N - 1) + geekonacci(A, B, C, N - 2) + geekonacci(A, B, C, N - 3);
+    }
+
+    // Solve the problem gicconacci without taking ABC
+
+    static int gic(int n){
+        if (n == 1) return 1;
+        if(n == 2) return 2;
+        if (n == 3) return 3;
+
+        return gic(n-1) + gic(n-2) + gic(n-3);
     }
 }
