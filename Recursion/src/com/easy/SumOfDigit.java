b28c97d8fdffda4cac96ae279b699d15d0092366
harshsinghh26 <harshsi343@gmail.com>
2025-04-20 12:54:16 +0530

    solve the sum Of Digit problem

--- Recursion/src/com/easy/SumOfDigit.java
+++ Recursion/src/com/easy/SumOfDigit.java
@@ -2,6 +2,15 @@ package com.easy;
 
 public class SumOfDigit {
     public static void main(String[] args) {
-        
+        System.out.println(sumOfDigit(45632));
+    }
+    static int sumOfDigit(int n){
+        int sum = 0;
+        if (n == 0){
+            return sum;
+        }
+        int rem = n % 10;
+        sum = rem + sumOfDigit(n/10);
+        return sum;
     }
 }
